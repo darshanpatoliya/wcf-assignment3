19566c27b528e8ce67a737a04562c12ad9b70e19
darshanpatoliya <darshanpatoliya05@gmail.com>
2022-11-29 12:47:41 -0500

    Service File Added

---
+++ WcfService1/WcfService1/IService1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string Insert(InsertTenant tenant);
+
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class InsertTenant
+    {
+        string name = string.Empty;
+        string email = string.Empty;
+        string rent = string.Empty;
+
+       
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        [DataMember]
+        public string Rent
+        {
+            get { return rent; }
+            set { rent = value; }
+        }
+    }
+}
---
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string Insert(InsertTenant tenant)
+        {
+            string message;
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-5H6RLHG;Initial Catalog=tenant;Persist Security Info=True;User ID=sa;Password=***********");
+            con.Open();     
+                SqlCommand cmd = new SqlCommand("Insert into tenant (Name, Rent, Email) values(@Name, @Rent, @Email)", con);
+            cmd.Parameters.AddWithValue("@Name", tenant.Name);
+            cmd.Parameters.AddWithValue("@Rent", tenant.Rent);
+            cmd.Parameters.AddWithValue("@Email", tenant.Email);
+
+            int tent = cmd.ExecuteNonQuery();
+            if(tent == 1)
+            {
+                message = "Successfully Inserted";
+            }
+            else
+            {
+                message = "Failed to Insert";
+            }
+            return message;
+        }
+
+
+        
+    }
+}
